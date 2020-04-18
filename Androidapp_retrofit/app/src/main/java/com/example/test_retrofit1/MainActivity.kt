19b918701837391e0b156122005ebbba6d4b1d89
akshaykulks995 <akshaykulkarni995@dev.azure.com>
2020-04-18 23:09:22 +0530

    Initial commit with basics of coroutines

--- Androidapp_retrofit/app/src/main/java/com/example/test_retrofit1/MainActivity.kt
+++ Androidapp_retrofit/app/src/main/java/com/example/test_retrofit1/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.test_retrofit1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
+
+    val tag = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        GlobalScope.launch {
+        Log.d(tag,"coroutines says hi,1st test ${Thread.currentThread().name}")
+        }
+
+
+        //optional to delay whenever necessary
+        runBlocking {
+            delay(2000)
+        }
+        Log.d(tag,"Main Thread test1 ${Thread.currentThread().name}")
     }
 }
