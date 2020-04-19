97e8537754583e811570e03eedf26b4e98d3c944
akshaykulks995 <akshaykulkarni995@dev.azure.com>
2020-04-19 14:04:31 +0530

    Suspend Function In Coroutines

--- Androidapp_retrofit/app/src/main/java/com/example/test_retrofit1/MainActivity.kt
+++ Androidapp_retrofit/app/src/main/java/com/example/test_retrofit1/MainActivity.kt
@@ -15,17 +15,41 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        GlobalScope.launch {
         Log.d(tag,"coroutines says hi,1st test ${Thread.currentThread().name}")
+
+        //statring with coroutines
+        GlobalScope.launch {
+//        Log.d(tag,"coroutines says hi,1st test ${Thread.currentThread().name}")
+            val networkCallAnswer = doNetworkCall()
+            val networkCallAnswer1 = doNetworkCall1()
+
+//            to display above values
+            Log.d(tag,networkCallAnswer)
+            Log.d(tag,networkCallAnswer1)
         }
 
+//        //optional to delay whenever necessary
+//        runBlocking {
+//            delay(2000)
+//        }
+//        Log.d(tag,"Main Thread test1 ${Thread.currentThread().name}")
 
-        //optional to delay whenever necessary
-        runBlocking {
-            delay(2000)
-        }
-        Log.d(tag,"Main Thread test1 ${Thread.currentThread().name}")
+
+    }
+    //suspend fun with delay init
+      suspend fun doNetworkCall():String{
+
+          delay(3000L)
+
+          return " This is the test for suspend function"
+
+    }
+
+    suspend fun doNetworkCall1():String{
+
+        delay(3000L)
+
+        return " This is the test for suspend function1"
+
     }
 }
